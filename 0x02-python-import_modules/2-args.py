15789d836e3b5ee881a0d88d69cd80beac6fdafe
longinus4 <chijiokelonginus4@gmail.com>
2024-01-05 18:08:03 +0100

    program that prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 from sys import argv
 
 if __name__ == "__main__":
-    args_len = len(argv) - 1  # Exclude the script name from the count
+    args_len = len(argv) - 1
 
     print("{} argument{}{}:".format(args_len, 's' if args_len !=
           1 else '', '.' if args_len == 0 else ''))
