d64cc70d0d575953279cad88d677baffb12a70e8
longinus4 <chijiokelonginus4@gmail.com>
2024-01-12 09:57:06 +0100

    corrections made to 6-print_sorted_dictionary file

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,4 @@ def print_sorted_dictionary(a_dictionary):
     sort_list = sorted(a_dictionary.keys())
     for key in sort_list:
         value = a_dictionary[key]
-        print(f"{key} : {value}")
+        print(f"{key}: {value}")
--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    # for key in a_dictionary:
     if key in a_dictionary:
         a_dictionary[key] = value
     a_dictionary[key] = value
