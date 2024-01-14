2e425921197ee7ed0957309e8064ab3852799d7f
longinus4 <chijiokelonginus4@gmail.com>
2024-01-14 22:48:29 +0100

    removed uncessary declaration

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,6 @@
 
 def best_score(a_dictionary):
     if a_dictionary is not None:
-        highnum = {}
-
         for key, value in a_dictionary.items():
             maxnum = max(a_dictionary)
         return maxnum
