95d04f51d9d62dd32224e76b643dedefe1c66906
chijioke <chijiokelonginus4@gmail.com>
2024-01-28 15:21:53 +0100

    added module documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 
+"""
+This module defines the Square class.
+
+The Square class represents a basic empty square.
+
+Attributes:
+    None.
+
+Methods:
+    None.
+"""
+
 class Square():
 	"""represents empty Square class"""
 
