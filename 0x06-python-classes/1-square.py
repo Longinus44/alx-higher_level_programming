6bff52d5950fd5f281e4df56e65d139923b60f01
chijioke <chijiokelonginus4@gmail.com>
2024-01-28 16:09:54 +0100

    added checks for size type and value

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -21,4 +21,9 @@ class Square:
         Parameters:
             size (int): The size of the square.
         """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be greater than or  0")
         self.__size = size
