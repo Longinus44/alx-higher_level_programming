586c58ae38b85150626e3389bd680ca79966c125
chijioke <chijiokelonginus4@gmail.com>
2024-01-29 01:21:45 +0100

    added neccesary doc

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
 
 import math
+"""
+A magical class for performing calculations on a given radius.
 
+    Attributes:
+    __radius (float): The radius for calculations.
+"""
 class MagicClass:
+    """ represent class magicclass"""
+   
+
     def __init__(self, radius):
-        self.__radius = 0  # Default value
+        """ 
+        Initializes the MagicClass instance with a given radius.
+
+        Args:
+        radius (int or float): The radius for calculations.
+
+        Raises:
+        TypeError: If the radius is not of type int or float.
+        """
+        self.__radius = 0
 
         if type(radius) is not int or type(radius) is not float:
             raise TypeError('radius must be a number')
@@ -12,7 +29,20 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
+        """
+        Calculates and returns the area based on the radius.
+
+        Returns:
+        float: The calculated area.
+        """
         return 2 ** self.__radius * math.pi
 
     def circumference(self):
+
+        """
+        Calculates and returns the circumference based on the radius.
+
+        Returns:
+        float: The calculated circumference.
+        """
         return 2 * math.pi * self.__radius
