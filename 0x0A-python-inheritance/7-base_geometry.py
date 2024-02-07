610a74aa8d4ef7f5d46bcf3a8ac0d5860eb9d5e1
chijioke <chijiokelonginus4@gmail.com>
2024-02-07 05:23:22 +0100

    BaseGeometry class that handles exception

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""This module defines BaseGeometry class"""
+
+
+class BaseGeometry:
+    """
+    A base class representing geometric shapes.
+    """
+
+    def area(self):
+        """
+        Calculate the area of the geometric shape.
+
+        Raises:
+            Exception: This method is not implemented in the base class.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validate if the value is an integer and greater than 0.
+
+        Args:
+            name (str): The name of the value being validated.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
