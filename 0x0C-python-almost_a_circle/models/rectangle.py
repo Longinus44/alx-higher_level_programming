f623918e8c31c679847671f523ee7466759d1af9
chijioke <chijiokelonginus4@gmail.com>
2024-02-09 02:56:41 +0100

    Update the class Rectangle by adding validation of all setter methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,52 +5,66 @@ from .base import Base
 
 
 class Rectangle(Base):
-    """The class Rectangle"""
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialises the class with the attributes """
         super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
-        self.__y = 0
+        self.__y = y
 
-        @property
-        def width(self):
-            """this defines a getter method for width"""
-            return self.width
+    @property
+    def width(self):
+        """this defines a getter method for width"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """defines a setter method for width"""
-            self.width = value
+    @width.setter
+    def width(self, value):
+        """defines a setter method for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """this defines a getter method for height"""
-            return self.height
+    @property
+    def height(self):
+        """this defines a getter method for height"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """defines a setter method for height"""
-            self.height = value
+    @height.setter
+    def height(self, value):
+        """defines a setter method for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
-        @property
-        def x(self):
-            """this defines a getter method for x"""
-            return self.x
+    @property
+    def x(self):
+        """this defines a getter method for x"""
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            """defines a setter method for x"""
-            self.x = value
+    @x.setter
+    def x(self, value):
+        """defines a setter method for x"""
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
-        @property
-        def y(self):
-            """this defines a getter method for width"""
-            return self.y
+    @property
+    def y(self):
+        """this defines a getter method for width"""
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            """defines a setter method for y"""
-            self.y = value
+    @y.setter
+    def y(self, value):
+        """defines a setter method for y"""
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """ area method that returns the rectangle area"""
+        return self.__width * self.__height
