aa09ded0f957f490947d10b418d7c451843927b7
chijioke <chijiokelonginus4@gmail.com>
2024-02-10 05:14:00 +0100

    Update the class Base by adding the static method def from_json_string(json_string): that returns the list of the JSON string representation json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -39,3 +39,11 @@ class Base:
         with open(filename, "w") as file:
             json_string = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
             file.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if not json_string:
+            return []
+        else:
+            return json.loads(json_string)
