75dbae8286d13fe199b93f0267958b7dc4ce5ca4
chijioke <chijiokelonginus4@gmail.com>
2024-03-15 03:21:34 +0100

    update made

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,8 @@
+-- Task: List all privileges of MySQL users user_0d_1 and user_0d_2 on the server (localhost).
+-- MySQL Version: 8.0.25
+
+-- SQL Query to list privileges of specified users
+
 SELECT
     user, 
     host, 
