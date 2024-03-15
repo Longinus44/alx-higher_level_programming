af0b8e34a6e21b8e2962aea0b22e86001e3617d7
chijioke <chijiokelonginus4@gmail.com>
2024-03-15 04:26:54 +0100

    List all privileges of MySQL users user_0d_1 and user_0d_2 on the server (localhost).

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,17 +1,6 @@
 -- Task: List all privileges of MySQL users user_0d_1 and user_0d_2 on the server (localhost).
--- MySQL Version: 8.0.25
 
 -- SQL Query to list privileges of specified users
 
-SELECT
-    user, 
-    host, 
-    db, 
-    table_name, 
-    column_name, 
-    routine_name, 
-    privilege_type 
-FROM 
-    mysql.db 
-WHERE 
-    (user = 'user_0d_1' OR user = 'user_0d_2');
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
