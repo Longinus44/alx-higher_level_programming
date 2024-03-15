c74f555746f798dbfd9e0857fd9bc9c65bf48eff
chijioke <chijiokelonginus4@gmail.com>
2024-03-15 04:41:16 +0100

    Create database hbtn_0d_2 and user user_0d_2 with limited privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,28 +1,9 @@
 -- Task: Create database hbtn_0d_2 and user user_0d_2 with limited privileges
--- MySQL Version: 8.0.25
 
--- Check if the database exists
-SELECT COUNT(*)
-FROM information_schema.schemata
-WHERE schema_name = 'hbtn_0d_2' INTO @db_exists;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
--- If database doesn't exist, create it
-IF @db_exists = 0 THEN
-    CREATE DATABASE hbtn_0d_2;
-END IF;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
--- Check if the user exists
-SELECT COUNT(*)
-FROM mysql.user
-WHERE user = 'user_0d_2' AND host = 'localhost' INTO @user_exists;
-
--- If user doesn't exist, create it
-IF @user_exists = 0 THEN
-    CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-END IF;
-
--- Grant SELECT privilege to the user on the database
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
--- Flush privileges to apply changes
 FLUSH PRIVILEGES;
