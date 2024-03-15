1de029d324bc7511fcdc9a752faab1e4677920bc
chijioke <chijiokelonginus4@gmail.com>
2024-03-15 05:16:10 +0100

    changes made

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,6 @@
 -- Task: List all cities of California from the database hbtn_0d_usa
 
-SELECT cities.*
-FROM cities, states
-WHERE cities.state_id = states.id
-  AND states.name = 'California'
-ORDER BY cities.id ASC;
+SELECT * FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY id ASC;
+
