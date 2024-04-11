844eab550239cf49d07ca1a06f52ed05fe1d92a0
chijioke <chijiokelonginus4@gmail.com>
2024-04-11 01:55:26 +0100

    script that displays strings

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let cIsFun, python, javascript;
+
+cIsFun = 'C is fun';
+python = 'Python is cool';
+javascript = 'JavaScript is amazing';
+
+console.log(cIsFun)
+console.log(python);
+console.log(javascript);
