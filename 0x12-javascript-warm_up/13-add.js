5d32ad203af7abe624f7de1666427511a7640fc1
chijioke <chijiokelonginus4@gmail.com>
2024-04-12 02:11:58 +0100

    update made

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,7 +1,14 @@
 #!/usr/bin/node
 
 function add (a, b) {
-  return a + b;
+  const numA = parseInt(a, 10);
+  const numB = parseInt(b, 10);
+
+  if (isNaN(numA) || isNaN(numB)) {
+    console.error('Error: Please provide valid numbers');
+    return;
+  }
+  return numA + numB;
 }
 
 module.export = add;
