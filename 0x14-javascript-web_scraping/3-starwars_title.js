18001df1bf69dc7c362f09fa6499ad953581a1a6
chijioke <chijiokelonginus4@gmail.com>
2024-05-21 18:58:34 +0100

    feat: add script to print the title of a Star Wars movie by episode number
    - Added `getStarWarsMovie.js` script to fetch and print the title of a Star Wars movie based on the given episode number.
    - Utilized the Star Wars API endpoint 'https://swapi-api.alx-tools.com/api/films/:id'.
    - Implemented error handling to print error object or status code if the request fails.
    - Followed semistandard and Airbnb style guide with added semicolons.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,28 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Please provide a movie ID as the first argument.');
+  process.exit(1);
+}
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Failed to retrieve data: ${response.statusCode}`);
+    return;
+  }
+
+  const data = JSON.parse(body);
+
+  console.log(`Title: ${data.title}`);
+});
